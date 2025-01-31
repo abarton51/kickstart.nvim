9709db05b7ee350c21c803da2bf48af0d32c99e5
abarton51 <abarton51@users.noreply.github.com>
2025-01-31 10:40:50 -0500

    add screenshot of config

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -4,11 +4,23 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      sections = {
+        {
+          section = 'terminal',
+          cmd = 'chafa ~/.config/marmot_retro.jpg -C on --size 50x50 -c full --color-space rgb --fill none; sleep .1',
+          random = 10,
+          pane = 2,
+          indent = 4,
+          height = 25,
+        },
+        { section = 'header' },
+        { section = 'keys', gap = 1, padding = 1 },
+        { section = 'startup' },
+      },
+    },
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
